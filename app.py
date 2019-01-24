ecac49a2a662db261123c848374a3edbb3086f44
engysamaan <40504997+engysamaan@users.noreply.github.com>
2019-01-23 23:05:31 -0500

    parent html

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 from flask import Flask, render_template, request, redirect, jsonify,  url_for
 # from flask_mysqldb import MySQL
@@ -8,7 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect
-import subway
+# import subway
 #################################################
 # Database Setup
 #################################################
@@ -30,35 +28,32 @@ import subway
 # for column in columns:
 #     print(column["name"], column["type"])
 
-=======
-from flask import Flask, render_template, redirect,url_for
-from flask_pymongo import PyMongo
-import subway
->>>>>>> Engy
+# =======
+# from flask import Flask, render_template, redirect,url_for
+# from flask_pymongo import PyMongo
+# import subway
+# >>>>>>> Engy
 
 # create instance of Flask app
 app = Flask(__name__)
 
-<<<<<<< HEAD
-# config db
-# db = yaml.load(open('db.yaml'))
-# app.config['MYSQL_HOST'] = db['mysql_host']
-# app.config['MYSQL_USER'] = db['mysql_user']
-# app.config['MYSQL_PASSWORD'] = db['mysql_password']
-# app.config['MYSQL_DB'] = db['mysql_db']
-=======
-=======
-from flask import Flask, render_template, redirect,url_for
-from flask_pymongo import PyMongo
-import subway
+# # config db
+# # db = yaml.load(open('db.yaml'))
+# # app.config['MYSQL_HOST'] = db['mysql_host']
+# # app.config['MYSQL_USER'] = db['mysql_user']
+# # app.config['MYSQL_PASSWORD'] = db['mysql_password']
+# # app.config['MYSQL_DB'] = db['mysql_db']
+# =======
+# from flask import Flask, render_template, redirect,url_for
+# from flask_pymongo import PyMongo
+# import subway
 
-# create instance of Flask app
-app = Flask(__name__)
+# # create instance of Flask app
+# app = Flask(__name__)
 
->>>>>>> ef67ea1645c13f1d4664bc22201d0f71a1d40d22
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/app.py"
-mongo = PyMongo(app)
+# # Use flask_pymongo to set up mongo connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/app.py"
+# mongo = PyMongo(app)
 
 # posts = [    {
 #         'author': 'Corey Schafer',
@@ -72,7 +67,6 @@ mongo = PyMongo(app)
 #         'content': 'Second post content',
 #         'date_posted': 'April 21, 2018'
 #     }]
->>>>>>> Engy
 
 # mysql = MySQL(app)
 
